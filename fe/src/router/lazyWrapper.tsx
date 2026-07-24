7954877923acb365cdcabada06082038368249d0
Rene Levec <renelevec@Mac.fritz.box>
2026-07-24 13:54:06 +0200

    comments

--- fe/src/router/lazyWrapper.tsx
+++ fe/src/router/lazyWrapper.tsx
@@ -1,4 +1,5 @@
 import { Suspense } from "react";
+
 import type { ComponentType, LazyExoticComponent } from "react";
 
 type Props = {
---
+++ fe/src/store/useFormBuilderStore.ts
@@ -0,0 +1,10 @@
+import { create } from "zustand";
+
+//local state from formbuilder should be switched to global
+//templates modal will also populate this state on template edit
+
+const useFormBuilderStore = create(() => {
+
+})
+
+export default useFormBuilderStore
